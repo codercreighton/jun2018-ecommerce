cc91462f71330de570ea181cb9c71087d9d5a93d
codercreighton <codercreighton@gmail.com>
2018-07-31 09:14:20 -0400

    added item pages and devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+
+  before_action :categories, :brands
+
+# created data for our categories dropdown
+  def categories
+  	@categories = Category.order(:name)
+  end	
+
+# creates data for our brands dropdown
+  def brands
+  	@brands = Product.pluck(:brand).sort.uniq
+  end	
 end
